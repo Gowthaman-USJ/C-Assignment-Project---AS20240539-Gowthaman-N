9947b27db895a610e6e0795d5c0af5b3be333be9
Gowthaman N <as20240539@sci.sjp.ac.lk>
2025-10-20 10:43:36 +0530

    Created Distance Management Function
    Distance Management function will edit existing distance between two cities and display the distance table

--- AS20240539.C
+++ AS20240539.C
@@ -6,18 +6,102 @@
 #include <string.h>
 #include <stdbool.h>
 
-int citymanagement(FILE *file1);
+
+#define MAX_CITIES 30
+
+
+void menu(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
+int selcchoice(char vehicletype[][6],int vehicledetails[][4],int choice,char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
+int readfromfiles(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
+int citymanagement();
+int distancemanagement(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
+int printdistable(char vehicletype[][6],int vehicledetails[][4], char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
+int  vehiclemanagement(char vehicletype[][6], int vehicledetails[][4]);
+void deliveryorder(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]);
 
 int main(){
-FILE *file1;
-citymanagement(file1);
+//FILE *file1 = fopen("cities.txt","r");
+//FILE *file2 = fopen("UniqueCode.txt","r");
+//FILE *file3 = fopen("distance.txt","r");
+char vehicletype[][6] = {"Van","Truck","Lorry"};
+int vehicledetails[][4] = {{1000,30,60,12},{5000,40,50,6},{10000,80,45,4}};
+
+int numberarray[MAX_CITIES][MAX_CITIES]; char uniquecodearr[MAX_CITIES][10]; char citiesarr[MAX_CITIES][MAX_CITIES];
+
+readfromfiles(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+
+
+}
+void menu(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]){
+    int choice;
+
+    printf("\n--Menu--\n");
+    printf("1.City Management(Add,edit or rename a city\n");
+    printf("2.Distance Management\n");
+    printf("3.Display the vehicle type table\n");
+    printf("4.Place a delivery order\n");
+    printf("Enter Choice : ");
+    scanf("%d",&choice);
+    selcchoice(vehicletype,vehicledetails,choice,citiesarr,uniquecodearr,numberarray);
 }
 
-int citymanagement(FILE *file1){
-    int choice; char city[20];
+int selcchoice(char vehicletype[][6],int vehicledetails[][4],int choice,char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]){
+    switch(choice){
+    case 1:
+        citymanagement();
+        readfromfiles(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+        break;
+
+    case 2:
+        distancemanagement(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+        break;
+
+    case 3:
+        vehiclemanagement(vehicletype,vehicledetails);
+        break;
+    case 4:
+        deliveryorder(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+        break;
+
+
+}
+
+
+
+}
+int readfromfiles(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]){
+    FILE *file1 = fopen("cities.txt","r");
+    FILE *file2 = fopen("UniqueCode.txt","r");
+    FILE *file3 = fopen("distance.txt","r");
+    int i = 0;
+
+    while (fgets(citiesarr[i], sizeof(citiesarr[i]), file1)&& i<MAX_CITIES) {
+                i++;
+           }
+    fclose(file1);
+    i=0;
+     while (fgets(uniquecodearr[i], sizeof(uniquecodearr[i]), file2)&& i<MAX_CITIES) {
+                i++;
+           }
+    fclose(file2);
+    i=0;
+     for (int i = 0; i < 30;i++) {
+        for (int j = 0; j < 30; j++) {
+            fscanf(file3, "%d", &numberarray[i][j]);
+        }
+    }
+    fclose(file3);
+    menu(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+    return 0;
+
+}
+int citymanagement(){
+    int choice; char city[MAX_CITIES],buffer[MAX_CITIES];
+     FILE *file1; FILE *temp1;
 
     printf("1.Add a new city\n");
-    printf("2.Rename or remove a city\n");
+    printf("2.Rename a city\n");
+    printf("3.Remove a city\n");
     printf("Enter Choice : ");
     scanf("%d",&choice);
     getchar();
@@ -25,74 +109,121 @@ int citymanagement(FILE *file1){
 
         file1 = fopen("cities.txt","a");
         printf("Enter City Name: ");
-        fgets(city,20,stdin);
+        fgets(city,sizeof(city),stdin);
         fputs(city,file1);
         fclose(file1);
+        printf("Successfully Added %.*s to the list\n",(int)strlen(city)-1,city);
     }
     else if(choice == 2){
-        int choice2;  FILE *temp1;
-        char buffer[20];
 
         temp1 = fopen("Temporary.txt","w");
         file1 = fopen("cities.txt","r");
 
-        printf("1.Rename a city\n");
-        printf("2.Remove a city\n");
-        printf("Enter Choice: ");
-        scanf("%d",&choice2);
-        getchar();
+        char oldname[MAX_CITIES],newname[MAX_CITIES];
 
-        if(choice2 == 1){
-            char oldname[20]; char newname[20];
+        printf("Enter the city to be renamed : ");
+        fgets(oldname,sizeof(oldname),stdin);
 
-            printf("Enter the city to be renamed : ");
-            fgets(oldname,20,stdin);
-            oldname[strlen(oldname)] = '\0';
+        while (fgets(buffer, sizeof(buffer), file1)){
+            if(strcmp(buffer,oldname)==0){
+                printf("Enter the new name:  ");
+                fgets(newname,sizeof(newname),stdin);
+                fputs(newname, temp1);
 
-            while (fgets(buffer, 20, file1)){
-                if(strcmp(buffer,oldname)==0){
-                    printf("Enter the new name:  ");
-                    fgets(newname,20,stdin);
-
-                    fputs(newname, temp1);
-                    fputs("\n",temp1);
+                printf("\nSuccessfully renamed %.*s to %.*s\n",(int)strlen(oldname)-1,oldname,(int)strlen(newname)-1,newname);
                 }
-                else{
-                    fputs(buffer,temp1);
-                    printf("\nSuccessfully renamed %.*s to %.*s\n",(int)strlen(oldname)-1,oldname,(int)strlen(newname)-1,newname);
+            else{
+                fputs(buffer,temp1);
+
                 }
             }
-            fclose(file1);
-            fclose(temp1);
-            remove("cities.txt");
-            rename("Temporary.txt","cities.txt");
-
-
-            return 0;
+        fclose(file1);
+        fclose(temp1);
+        remove("cities.txt");
+        rename("Temporary.txt","cities.txt");
 
         }
 
-        else if(choice == 2){
-            char name[20];
+    else if(choice == 3){
+            char name[MAX_CITIES];
+
+            temp1 = fopen("Temporary.txt","w");
+            file1 = fopen("cities.txt","r");
 
             printf("Enter the city to be removed : ");
-            fgets(name,20,stdin);
-            while(fgets(buffer,20,file1)){
+            fgets(name,sizeof(name),stdin);
+            while(fgets(buffer,sizeof(buffer),file1)){
                 if(strcmp(buffer,name)!=0){
                     fputs(buffer,temp1);
                 }
             }
-            printf("Successfully removed city %.*s from the list\n",(int)strlen(name)-1,name);
+            printf("\nSuccessfully removed city %.*s from the list\n",(int)strlen(name)-1,name);
             fclose(file1);
             fclose(temp1);
             remove("cities.txt");
             rename("Temporary.txt","cities.txt");
-
             }
+            return 0;
+}
 
-        }
-        return 0;
+int distancemanagement(char vehicletype[][6],int vehicledetails[][4],char citiesarr[][MAX_CITIES],char uniquecodearr[][10],int numberarray[][MAX_CITIES]){
+    FILE *file2 = fopen("distance.txt","w");
+    FILE *temp1 = fopen("temporary.txt","r");
 
+    char fromcity[4]; char tocity[4]; char buffer[20];
+    int toindex=0,fromindex=0,choice;
+
+
+    printf("1.Input distances between two cities\n");
+    printf("2.Edit distances between two cities\n");
+    printf("3.Display the distance table\n");
+    printf("Enter Choice : ");
+    scanf("%d",&choice);
+    getchar();
+
+    if(choice == 1){
+        //printf("still working");
     }
+    else if(choice == 2){
+        printf("Enter the first 3 letters of From City : ");
+        scanf("%s",fromcity);
+        printf("Enter the first 3 letters of To City : ");
+        scanf("%s",tocity);
+        for(int i=0;i<MAX_CITIES;i++){
+                if(strncmp(uniquecodearr[i],fromcity,3)==0){
+                    fromindex = i;
+                    break;
+                }
+        }
+        for(int i=0;i<MAX_CITIES;i++){
+                if(strncmp(uniquecodearr[i],tocity,3)==0){
+                    toindex = i;
+                    break;
+                }
+        }
+    int newdistance ;
+    printf("Enter new distance : ");
+    scanf("%d",&newdistance);
+    numberarray[fromindex][toindex] = newdistance;
+    numberarray[toindex][fromindex] = newdistance;
+
+    for(int i=0;i<MAX_CITIES;i++){
+            for(int j=0;j<MAX_CITIES;j++){
+                fprintf(file2, "%03d ", numberarray[i][j]);
+            }
+            fprintf(file2, "\n");
+    }
+    fclose(file2);
+    printf("Distance Changed Successfully\n");
+    menu(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+    return 0;
+    }
+    else if(choice == 3){
+        printdistable(vehicletype,vehicledetails,citiesarr,uniquecodearr,numberarray);
+    }
+}
+
+
+
 
 
